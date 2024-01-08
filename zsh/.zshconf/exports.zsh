8cf71fc0dcdcab5eca0c7c41b4d62985b323657b
philae-ael <1511446+philae-ael@users.noreply.github>
2024-01-09 00:00:38 +0100

    add less colors

--- zsh/.zshconf/exports.zsh
+++ zsh/.zshconf/exports.zsh
@@ -1,2 +1,6 @@
+export PAGER='most'
 export PYTHONSTARTUP=/home/tristan/.pythonstartup.py
 export RANGER_LOAD_DEFAULT_RC=FALSE
+
+export LESSOPEN='||-pygmentize -g %s' # Use pygmentise to color output
+export LESS=' -R -N'  #Allow color and lines numbers
